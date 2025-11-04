0a07d2d72181343f49c281de1b5e209020d71632
KavishkaCeyOnline <KavishkaCeyOnline@gmail.com>
2025-11-04 09:23:20 +0530

    Add project files.

---
+++ PersonalLibraryManager/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PersonalLibraryManager.Startup))]
+namespace PersonalLibraryManager
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
